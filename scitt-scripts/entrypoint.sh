a8cc2b60acdbb76e44e94dbfc0b8bc6c08e12e3a
steve lasker <stevenlasker@hotmail.com>
2024-01-23 17:34:47 -0800

    Fix args
    Signed-off-by: steve lasker <stevenlasker@hotmail.com>

--- scitt-scripts/entrypoint.sh
+++ scitt-scripts/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 
-echo "scitt-client_id:       " ${1}
-echo "scitt-scitt-secret:    " ${2}
-echo "signed-statement-file: " ${3}
-echo "feed:                  " ${4}
+# echo "scitt-client_id:       " ${1}
+# echo "scitt-scitt-secret:    " ${2}
+# echo "signed-statement-file: " ${3}
+# echo "feed:                  " ${4}
 
-echo "Create an access token"
+# echo "Create an access token"
 ./create-token.sh ${1} ${2}
 
-echo "Test permissions with the assets API"
-./query-assets.sh
+# echo "Test permissions with the assets API"
+# ./query-assets.sh
+
+./create_signed_statement.py
