1c056424b52372d713f8e35d3526efd519fd2310
steve lasker <stevenlasker@hotmail.com>
2024-01-25 14:30:52 -0800

    Fix token references
    Signed-off-by: steve lasker <stevenlasker@hotmail.com>

--- scitt-scripts/create-token.sh
+++ scitt-scripts/create-token.sh
@@ -3,16 +3,14 @@
 # echo "scitt-client_id:       " ${1}
 # echo "scitt-scitt-secret:    " ${2}
 # echo "token-file:            " ${3}
+
 if [ -z "$3" ]; then
   TOKEN_FILE=$HOME/.datatrails/bearer-token.txt
+  mkdir -p $HOME/.datatrails
+  chmod 0700 $HOME/.datatrails
 else
-  echo ${3}
+  TOKEN_FILE=${3}
 fi
-
-mkdir -p $HOME/.datatrails
-chmod 0700 $HOME/.datatrails
-
-echo $TOKEN_FILE
 
 RESPONSE=$(curl -s -S https://app.datatrails.ai/archivist/iam/v1/appidp/token \
             --data-urlencode "grant_type=client_credentials" \
@@ -23,9 +21,9 @@ if [[ $RESPONSE == *"access_token"* ]]; then
   #rm $TOKEN_FILE
 
   TOKEN=$(echo -n $RESPONSE | jq -r .access_token)
-  
+
   echo Authorization: Bearer $TOKEN > $TOKEN_FILE
-  # cat $TOKEN_FILE
+
 else
   echo $RESPONSE
   exit -1
