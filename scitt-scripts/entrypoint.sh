2ba4b06a1f15d296f2bab53f1ed0d8de19f34653
Corey Bonnell <corey.bonnell@digicert.com>
2024-03-04 17:09:28 -0500

    Clean up operation polling logic

--- scitt-scripts/entrypoint.sh
+++ scitt-scripts/entrypoint.sh
@@ -19,6 +19,8 @@ echo "PWD: $PWD"
 
 ls -la $TOKEN_FILE
 
+echo "Sign statement with key protected in Software Trust Manager"
+
 python /scripts/create_signed_statement.py \
   --feed ${3} \
   --payload-file ${4} \
@@ -27,17 +29,16 @@ python /scripts/create_signed_statement.py \
 
 echo "SCITT Register to https://app.datatrails.ai/archivist/v1/publicscitt/entries"
 
-RESPONSE=$(curl -X POST -H @$TOKEN_FILE \
+OPERATION_ID=$(curl -X POST -H @$TOKEN_FILE \
                 --data-binary @$SIGNED_STATEMENT_FILE \
-                https://app.datatrails.ai/archivist/v1/publicscitt/entries)
-echo $RESPONSE
+                https://app.datatrails.ai/archivist/v1/publicscitt/entries | jq -r .operationID)
 
-echo "OPERATION_ID :" $OPERATION_ID | jq -r .operationID
+echo "OPERATION_ID :" $OPERATION_ID
 
-# echo "call: /scripts/check_operation_status.py"
-# python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE
+echo "call: /scripts/check_operation_status.py"
+python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE
 
-# ENTRY_ID=$(python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE)
-# echo "ENTRY_ID :" $ENTRY_ID
+ENTRY_ID=$(python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE)
+echo "ENTRY_ID :" $ENTRY_ID
 
 # curl https://app.datatrails.ai/archivist/v2/publicassets/-/events?event_attributes.feed_id=$FEED | jq
