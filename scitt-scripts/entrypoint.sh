73965c72a9c77f9f4198dfd5a56edd0c7ff2b3b4
steve lasker <stevenlasker@hotmail.com>
2024-03-06 12:26:08 -0800

    Fix check-operation-status
    Signed-off-by: steve lasker <stevenlasker@hotmail.com>

--- scitt-scripts/entrypoint.sh
+++ scitt-scripts/entrypoint.sh
@@ -12,16 +12,17 @@
 
 SIGNED_STATEMENT_FILE=./${6}
 TOKEN_FILE="./bearer-token.txt"
+FEED=${3}
 
 # echo "Create an access token"
-/scripts/create-token.sh ${1} ${2} $TOKEN_FILE
+./scitt-scripts/create-token.sh ${1} ${2} $TOKEN_FILE
 
 ls -a
 echo "PWD: $PWD"
 
 ls -la $TOKEN_FILE
 
-python /scripts/create_signed_statement.py \
+python ./scitt-scripts/create_signed_statement.py \
   --feed ${3} \
   --payload ${4} \
   --content-type ${5} \
@@ -34,14 +35,14 @@ echo "SCITT Register to https://app.datatrails.ai/archivist/v1/publicscitt/entri
 RESPONSE=$(curl -X POST -H @$TOKEN_FILE \
                 --data-binary @$SIGNED_STATEMENT_FILE \
                 https://app.datatrails.ai/archivist/v1/publicscitt/entries)
-echo $RESPONSE
 
-echo "OPERATION_ID :" $OPERATION_ID | jq -r .operationID
+OPERATION_ID=$(echo $RESPONSE | jq  -r .operationID)
+echo "OPERATION_ID: $OPERATION_ID"
 
-# echo "call: /scripts/check_operation_status.py"
-# python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE
+# echo "call: /scitt-scripts/check_operation_status.py"
+# python ./scitt-scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE
 
-# ENTRY_ID=$(python /scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE)
-# echo "ENTRY_ID :" $ENTRY_ID
+# RESPONSE=$(python ./scitt-scripts/check_operation_status.py --operation-id $OPERATION_ID --token-file-name $TOKEN_FILE)
+# ENTRY_ID=$(echo $RESPONSE | jq  -r .entryID)
 
 # curl https://app.datatrails.ai/archivist/v2/publicassets/-/events?event_attributes.feed_id=$FEED | jq
