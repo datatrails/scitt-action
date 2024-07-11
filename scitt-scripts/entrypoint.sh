233577b5c9c3d5b898ddb3e5a4415bbd4eeae010
steve lasker <stevenlasker@hotmail.com>
2024-07-11 13:26:39 -0700

    reorder params, add location, name variables
    Signed-off-by: steve lasker <stevenlasker@hotmail.com>

--- scitt-scripts/entrypoint.sh
+++ scitt-scripts/entrypoint.sh
@@ -1,36 +1,47 @@
 #!/bin/bash -l
 
 # Uncomment for debugging
-# echo "datatrails-client_id:    " ${1}
-# echo "datatrails-secret:       " ${2}
-# echo "subject:                 " ${3}
-# echo "payload:                 " ${4}
-# echo "content-type:            " ${5}
-# echo "signed-statement-file:   " ${6}
-# echo "receipt-file:            " ${7}
-# echo "signing-key-file:        " ${8}
-# echo "issuer:                  " ${9}
+# echo "content-type:            " ${1}
+# echo "datatrails-client_id:    " ${2}
+# echo "datatrails-secret:       " ${3}
+# echo "issuer:                  " ${4}
+# echo "subject:                 " ${5}
+# echo "payload_file:             " ${6}
+# echo "payload-location:        " ${7}
+# echo "receipt-file:            " ${8}
+# echo "signed-statement-file:   " ${9}
+# echo "signing-key-file:        " ${10}
+# echo "skip-receipt:            " ${11}
 
-SIGNED_STATEMENT_FILE=./${6}
+CONTENT_TYPE=${1}
+DATATRAILS_CLIENT_ID=${2}
+DATATRAILS_SECRET_ID=${3}
+ISSUER=${4}
+SUBJECT=${5}
+PAYLOAD=${6}
+PAYLOAD_LOCATION=${7}
+RECEIPT_FILE=${8}
+SIGNED_STATEMENT_FILE=${9}
+SIGNING_KEY_FILE=${10}
+
 TOKEN_FILE="./bearer-token.txt"
-SUBJECT=${3}
+
 
 echo "Create an access token"
-/scripts/create-token.sh ${1} ${2} $TOKEN_FILE
+/scripts/create-token.sh ${DATATRAILS_CLIENT_ID} ${DATATRAILS_SECRET_ID} $TOKEN_FILE
 
 # ls -a
-# echo "PWD: $PWD"
 
 # ls -la $TOKEN_FILE
 
 echo "Create a Signed Statement, hashing the payload"
 python /scripts/create_hashed_signed_statement.py \
-  --subject ${3} \
-  --payload ${4} \
-  --content-type ${5} \
+  --subject $SUBJECT \
+  --payload $PAYLOAD_FILE \
+  --content-type $CONTENT_TYPE \
   --output-file $SIGNED_STATEMENT_FILE \
-  --signing-key-file ${8} \
-  --issuer ${9}
+  --signing-key-file $SIGNING_KEY_FILE \
+  --issuer $ISSUER
 
 echo "Register the SCITT SIgned Statement to https://app.datatrails.ai/archivist/v1/publicscitt/entries"
 
