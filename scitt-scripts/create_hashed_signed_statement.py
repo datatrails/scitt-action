9598c415a366885c7d1355942025e043ff511f8c
steve lasker <stevenlasker@hotmail.com>
2024-11-07 10:20:29 +0000

    Change CWT_CLAIMS from 13 to 15
    Signed-off-by: steve lasker <stevenlasker@hotmail.com>

--- scitt-scripts/create_hashed_signed_statement.py
+++ scitt-scripts/create_hashed_signed_statement.py
@@ -20,7 +20,7 @@ from ecdsa import SigningKey, VerifyingKey
 
 # CWT header label comes from version 4 of the scitt architecture document
 # https://www.ietf.org/archive/id/draft-ietf-scitt-architecture-04.html#name-issuer-identity
-HEADER_LABEL_CWT = 13
+HEADER_LABEL_CWT = 15
 
 # Various CWT header labels come from:
 # https://www.rfc-editor.org/rfc/rfc8392.html#section-3.1
